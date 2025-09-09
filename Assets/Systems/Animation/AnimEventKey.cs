4c8d075f9779c0d23e34ea8563c4f149ac0f039a
PT <103441012+wejusdgfsJKA@users.noreply.github.com>
2025-09-09 13:07:33 +0300

    Animation events are now identified by a SO.

---
+++ Assets/Systems/Animation/AnimEventKey.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+namespace Animation
+{
+    [System.Serializable]
+    [CreateAssetMenu(menuName = "Animation/Event Key")]
+    public class AnimEventKey : ScriptableObject
+    {
+
+    }
+}
--- Assets/Systems/Animation/Events.cs
+++ Assets/Systems/Animation/Events.cs
@@ -6,12 +6,12 @@ namespace Animation
     [System.Serializable]
     public struct CustomAnimEvent : IEvent
     {
-        [field: SerializeField] public int ID { get; private set; }
+        [field: SerializeField] public AnimEventKey ID { get; private set; }
     }
     [System.Serializable]
     public struct AnimEvent
     {
         public UnityEvent Event;
-        [field: SerializeField] public int ID { get; private set; }
+        [field: SerializeField] public AnimEventKey ID { get; private set; }
     }
 }

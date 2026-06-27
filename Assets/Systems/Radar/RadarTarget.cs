9016d298ceef548a563c9daa0186f0f1cc14af64
PT <103441012+wejusdgfsJKA@users.noreply.github.com>
2026-06-27 11:25:24 +0300

    Updated RadarTarget.

--- Assets/Systems/Radar/RadarTarget.cs
+++ Assets/Systems/Radar/RadarTarget.cs
@@ -7,11 +7,11 @@ namespace Radar
         [field: SerializeField] public float RadarSignature { get; protected set; } = 1f;
         protected void OnEnable()
         {
-            RadarSystem.TryGetInstance(true).RegisterTarget(this);
+            RadarSystem.TryGetInstance()?.RegisterTarget(this);
         }
         protected void OnDisable()
         {
-            RadarSystem.TryGetInstance().UnregisterTarget(this);
+            RadarSystem.TryGetInstance()?.UnregisterTarget(this);
         }
     }
 }
